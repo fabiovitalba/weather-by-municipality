39aefb1c753e8dede115e492e822881f01d75f28
Fabio.Vitalba <fabvit9@gmail.com>
2024-05-22 17:30:58 +0200

    Updated with latests changes from ODH webcomponent

--- src/municipalities.js
+++ src/municipalities.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import captions from "./captions";
 import { formatDateInLang } from "./utils";
 
